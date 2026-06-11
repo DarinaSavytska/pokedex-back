2ab6e3520ed6d0610508228f3f19ec7cfe23c1b8
Zero_Citado <coldshine@gmail.com>
2026-06-11 20:49:57 +0300

    Added cors

--- pages/api/pokemon.js
+++ pages/api/pokemon.js
@@ -1,17 +1,16 @@
 export default async function handler(req, res) {
-    console.log("API route called");
-    try {
-        // const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
-        // const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001, http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        // Можеш обробити дані тут: відфільтрувати, об’єднати кілька запитів тощо
+    try {
         const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1");
         const respons = await fetch("https://pokeapi.co/api/v2/pokemon?offset=1&limit=1");
+
         const data = await response.json();
         const dat = await respons.json();
-        res.status(200).json([...data.results, ...dat.results]);
 
-        // res.status(200).json(data);
+        res.status(200).json([...data.results, ...dat.results]);
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch data" });
     }
