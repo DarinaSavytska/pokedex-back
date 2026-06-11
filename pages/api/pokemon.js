ce465f8ed9bace4ad90594794d9ba81143fb912a
Zero_Citado <coldshine@gmail.com>
2026-06-11 21:06:57 +0300

    Updated cors pages

--- pages/api/pokemon.js
+++ pages/api/pokemon.js
@@ -1,17 +1,31 @@
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001",
+  "https://pokedex-front.vercel.app"
+];
+
 export default async function handler(req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001, http://localhost:3000");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
 
-    try {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=2");
-        const respons = await fetch("https://pokeapi.co/api/v2/pokemon?offset=2&limit=3");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        const data = await response.json();
-        const dat = await respons.json();
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
 
-        res.status(200).json([...data.results, ...dat.results]);
-    } catch (error) {
-        res.status(500).json({ error: "Failed to fetch data" });
-    }
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=2");
+    const respons = await fetch("https://pokeapi.co/api/v2/pokemon?offset=2&limit=3");
+
+    const data = await response.json();
+    const dat = await respons.json();
+
+    res.status(200).json([...data.results, ...dat.results]);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
 }
