682b0579e1fbb336725c524f11ae94239c6fe00e
Zero_Citado <coldshine@gmail.com>
2026-06-14 12:26:51 +0300

    Create paging request

--- pages/api/pokemon.js
+++ pages/api/pokemon.js
@@ -1,11 +1,10 @@
 const allowedOrigins = [
-  "http://localhost:3000",
-  "http://localhost:3001",
-//   "https://pokedex-front.vercel.app",
+  // "http://localhost:3000",
+  // "http://localhost:3001",
   "https://darinasavytska.github.io"
 ];
 
-export default async function handler(req, res) {
+export default async function handler(req = 0, res) {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
@@ -18,15 +17,35 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
+  const limit = 25;
+
   try {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=2");
-    const respons = await fetch("https://pokeapi.co/api/v2/pokemon?offset=2&limit=3");
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?offset=${limit * req.query.page}&limit=${limit}`);
 
     const data = await response.json();
-    const dat = await respons.json();
 
-    res.status(200).json([...data.results, ...dat.results]);
+    const fullData = data.results.map((pokemon, idx) => {
+      const pokeId = `${idx + 1 + limit * req.query.page}`;
+      const pokeNum = (pokeId) => {
+        if ((pokeId) < 10) {
+          return `00${pokeId}`;
+        } else if ((pokeId) < 100) {
+          return `0${pokeId}`;
+        }
+
+        return `${pokeId}`;
+      };
+
+      return {
+        name: pokemon.name,
+        url: pokemon.url,
+        image: `https://projectpokemon.org/images/normal-sprite/${pokemon.name}.gif`,
+        miniImage: `https://serebii.net/pokedex-dp/icon/${pokeNum(pokeId)}.gif`
+      };
+    });
+
+    res.status(200).json(fullData);
   } catch (error) {
-    res.status(500).json({ error: "Failed to fetch data" });
+    res.status(500).json({ error: `Failed to fetch data, error: ${error.message}` });
   }
 }
